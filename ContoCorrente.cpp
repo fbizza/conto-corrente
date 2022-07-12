5283c34fbe6e0633c5a94cf7b84665f88c05cb39
bizza <francesco.bizzarri@stud.unifi.it>
2022-07-12 16:38:20 +0200

    Preparazione per integrare i test

--- ContoCorrente.cpp
+++ ContoCorrente.cpp
@@ -1,8 +1,8 @@
 #include "contocorrente.h"
 #include <ctime>
-#include <QIODevice>
-#include <QFile>
-#include <QTextStream>
+//#include <QIODevice>
+//#include <QFile>
+//#include <QTextStream>
 
 void ContoCorrente::preleva(int x) {
     if (Saldo - x > 0) {
@@ -11,28 +11,28 @@ void ContoCorrente::preleva(int x) {
     else throw "Saldo contabile non sufficiente!";
     time_t now = time(0);
     std::string dt = ctime(&now);
-    QString DataEOra = QString::fromStdString(dt);
-    QFile file("C:/Users/bizza/Desktop/Laboratorio di Programmazione/conto-corrente-gui/ListaMovimenti.txt");
-    if(file.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text)) {
-        QTextStream stream(&file);
-        stream << "\n" << DataEOra << "Sono stati prelevati " << x << "€";
-        stream << "\nNuovo saldo: " << Saldo << "€\n";
-        file.close();
-    }
+//    QString DataEOra = QString::fromStdString(dt);
+//    QFile file("C:/Users/bizza/Desktop/Laboratorio di Programmazione/conto-corrente-gui/ListaMovimenti.txt");
+//    if(file.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text)) {
+//        QTextStream stream(&file);
+//        stream << "\n" << DataEOra << "Sono stati prelevati " << x << "€";
+//        stream << "\nNuovo saldo: " << Saldo << "€\n";
+//        file.close();
+//    }
 }
 
 void ContoCorrente::versamentoBancomat(int x) {
     Saldo += x;
     time_t now = time(0);
     std::string dt = ctime(&now);
-    QString DataEOra = QString::fromStdString(dt);
-    QFile file("C:/Users/bizza/Desktop/Laboratorio di Programmazione/conto-corrente-gui/ListaMovimenti.txt");
-    if(file.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text)) {
-        QTextStream stream(&file);
-        stream << "\n" << DataEOra << "Sono stati versati " << x << "€";
-        stream << "\nNuovo saldo: " << Saldo << "€\n";
-        file.close();
-    }
+//    QString DataEOra = QString::fromStdString(dt);
+//    QFile file("C:/Users/bizza/Desktop/Laboratorio di Programmazione/conto-corrente-gui/ListaMovimenti.txt");
+//    if(file.open(QIODevice::WriteOnly | QIODevice::Append | QIODevice::Text)) {
+//        QTextStream stream(&file);
+//        stream << "\n" << DataEOra << "Sono stati versati " << x << "€";
+//        stream << "\nNuovo saldo: " << Saldo << "€\n";
+//        file.close();
+//    }
 }
 
 void ContoCorrente::diminuisciSaldo(int x) {
