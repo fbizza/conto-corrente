ff797ae11793add99d37e893bf5c024719d4b510
bizza <francesco.bizzarri@stud.unifi.it>
2022-07-13 14:33:05 +0200

    Aggiornamento demo main

--- main.cpp
+++ main.cpp
@@ -11,18 +11,13 @@ int main() {
         c.preleva(100);
         Bonifico b(c, 30);
         b.eseguiBonifico();
-        std::cout << c.getSaldo() << std::endl;
         c.preleva(30);
         Investimento i(c, 5);
         i.compraAzioni();
         Investimento ii(c, 500);
         ii.vendiAzioni();
-        std::cout << c.getSaldo() << std::endl;
-
     } catch (const char* messaggio) {
         std::cout << messaggio << std::endl;
     }
-
-
     return 0;
 }
